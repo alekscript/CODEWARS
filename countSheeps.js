dd7386cfdba1f11352fe71dd73b78b236a9203f7
Aleksandr Piatopal <piatopal.alex@gmail.com>
2022-10-07 10:34:57 +0300

    Update countSheeps.js
    add test array, fixed errors

--- countSheeps.js
+++ countSheeps.js
@@ -1,14 +1,15 @@
 function countSheeps(arrayOfSheep) {
-    TODO May the force be with you
    let count = 0;
-   for ( let i = 0; i  arrayOfSheep.length; i++){
+   for ( let i = 0; i < arrayOfSheep.length; i++){
      if ( arrayOfSheep[i] == true) {
        count+= 1;
-     } else if ( arrayOfSheep[i] == false){
+     } else {
        count = count;
-     } else if (arrayOfSheep[i] == null && arrayOfSheep[i] == undefined){
-       count = count;
-     }
+     } 
    }
    return count;
  }
+let array = [ true, true, false, false,
+              false, false, true, false];
+let sum = countSheeps(array);
+console.log(sum);
