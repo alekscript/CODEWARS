3100444579d682d960490b167b35cdde84847c7a
alekscript <piatopal.alex@gmail.com>
2022-10-17 14:37:28 +0300

    task who is boo?

---
+++ who-is-boo.js
@@ -0,0 +1,15 @@
+// Хто такий Boo
+// Перевірте, чи значення відноситься булевого тиру (логічне значення). Функція має повертати true or false.
+// Булеві примітиви - це коди true правда та false неправда.
+
+function booWho(bool) {
+    if( bool === true){
+      return true;
+    } else if (bool === false){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  booWho(null);
