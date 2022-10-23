90218b0fe9a47f22d315013c330349b13e3fc8f2
alekscript <piatopal.alex@gmail.com>
2022-10-23 13:13:20 +0300

    task sum differance

---
+++ sum-differance.js
@@ -0,0 +1,14 @@
+function sumOfDifferences(arr) {
+    if (arr.length == 1) {
+        return 0;
+    }   else if ( arr  == []) {
+            return [];
+        } else {
+    arr.sort((a,b) => b - a);
+    let sum = 0;
+        for (let i = 0; i < arr.length - 1; ++i) {
+          sum += arr[i] - arr[i + 1];
+        }
+      return sum;
+    }
+  }
